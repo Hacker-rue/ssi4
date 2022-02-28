ec2df242bc8feebc6ecffb35b3a8bab45e3b6c23
Hacker-rue <pavlik.soldatov.12@gmail.com>
2022-02-28 10:31:48 +0400

    1

--- contracts/VC.sol
+++ contracts/VC.sol
@@ -5,7 +5,7 @@ pragma AbiHeader expire;
 
 contract VC {
 
-    uint256 static id;
+    uint256 static _id;
 
     address _owner;
 
@@ -14,19 +14,34 @@ contract VC {
     mapping(uint8 => bytes) _value;
 
     constructor() public {
+        optional(TvmCell) optSalt = tvm.codeSalt(tvm.code());
+        require(optSalt.hasValue());
+        (address owner) = optSalt.get().toSlice().decode(address);
+        require(owner != address(0));
+        require(msg.value >= 0.2 ton);
+        tvm.rawReserve(0.1 ton, 0);
 
+        _owner = owner;
+
+        _owner.transfer({value: 0, flag: 128});
     }
 
     function setType(string Type) public onlyOwner() {
         _type = Type;
+
+        msg.sender.transfer({value: 0, flag: 64});
     }
 
     function setSize(uint8 size) public onlyOwner() {
         _size = size;
+
+        msg.sender.transfer({value: 0, flag: 64});
     }
 
     function setValue(uint8 number, bytes value) public onlyOwner() {
         _value[number] = value;
+
+        msg.sender.transfer({value: 0, flag: 64});
     }
 
     function destruct() public onlyOwner() {
---
+++ contracts/resolvers/VCResolver.sol
@@ -0,0 +1,37 @@
+pragma ton-solidity 0.49.0;
+
+pragma AbiHeader time;
+pragma AbiHeader expire; 
+
+import "../VC.sol";
+
+contract VCResolver {
+    TvmCell _codeVC;
+
+    function resolveCodeHashVC(address owner) public view returns(uint256) {
+        return tvm.hash(_buildVCCode(owner));
+    }
+
+    function resolveVC(address owner, uint256 id) public view returns(address addrVC) {
+        TvmCell code = _buildVCCode(owner);
+        TvmCell state = _buildVCState(code, id);
+        uint256 hashState = tvm.hash(state);
+        addrVC = address.makeAddrStd(0, hashState);
+    }
+
+    function _buildVCCode(address owner) internal view returns(TvmCell) {
+        TvmBuilder salt;
+        salt.store(owner);
+        return tvm.setCodeSalt(_codeVC, salt.toCell());
+    }
+
+    function _buildVCState(TvmCell code, uint256 id) internal view returns(TvmCell) {
+        return tvm.buildStateInit({
+            contr: VC,
+            varInit: _id: id,
+            code: code
+        });
+    }
+
+
+}
