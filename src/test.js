632b6de06df1769a535dd2c0246b6b0614a4879a
Hacker-rue <pavlik.soldatov.12@gmail.com>
2022-04-05 13:25:22 +0400

    test

--- src/test.js
+++ src/test.js
@@ -1,48 +1,9 @@
-const { Metadata } = require("./app")
+const 
 
+const moduleEver = require('./modules/moduleEver')
+const moduleVC = require('./modules/moduleVC')
+const moduleVP = require('./modules/moduleVP')
 
-
-
-async function testMetadataClass() {
-    returnMetadata = {
-        '@context': [],
-        id: '',
-        type: [],
-        issuer: '',
-        issuanceDate: '',
-        credentialSubject: { id: '', name: '' },
-        proof: {
-          type: 'Ed25519Signature2020',
-          created: '2021-11-13T18:19:39Z',
-          verificationMethod: 'https://example.edu/issuers/14#key-1',
-          proofPurpose: 'assertionMethod',
-          proofValue: 'z58DAdFfa9SkqZMVPxAQpic7ndSayn1PzZs6ZjWp1CktyGesjuTSwRdoWhAfGFCF5bppETSTojQCrfFPP2oumHKtz'
-        }
-    }
-
-
-    requiredParameters = [
-        "id",
-        "type",
-        "issuer",
-        "issuanceDate",
-        "credentialSubject",
-        "proof"
-    ]
-
-    credentialSubject = "\"credentialSubject\": {" +
-    "\"id\": \"\"," +
-    "\"name\": \"\" },"
-
-    uri = "https://www.w3.org/2018/credentials/v1"
-
-    var _Metadata = new Metadata(uri, requiredParameters, credentialSubject)
-
-    JsonMetadata = await _Metadata.getMetadataVC()
-
-    if(JSON.stringify(JsonMetadata) == JSON.stringify(returnMetadata)) {
-        console.log("Функция работает правильно!")
-    }
-}
-
-testMetadataClass()
+const statusVCAPI = require('./modules/Everscale/statusVCAPI')
+const statusVCRootAPI = require('./modules/Everscale/statusVCRootAPI')
+const VCAPI = require('./modules/Everscale/VCAPI')
