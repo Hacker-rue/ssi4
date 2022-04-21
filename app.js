13d5094f55b728f55e9edfaf7b3d7c19b3737de7
Hacker-rue <pavlik.soldatov.12@gmail.com>
2022-04-21 11:54:27 +0400

    1

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+const statusVCAPI = require('./src/modules/Everscale/statusVCAPI')
+const statusVCRootAPI = require('./src/modules/Everscale/statusVCRootAPI')
+const VCAPI = require('./src/modules/Everscale/statusVCRootAPI')
+
+const moduleEver = require('./src/modules/moduleEver')
+const moduleVC = require('./src/modules/moduleVC')
+const moduleVP = require('./src/modules/moduleVP')
+
 module.exports = {
-    
+    statusVCAPI,
+    statusVCRootAPI,
+    VCAPI,
+    moduleEver,
+    moduleVC,
+    moduleVP
 }
